8401fb6a8bf5300c36be35ee4aa982fb00d22b7d
Lukepancrazi9 <lukepancrazi@gmail.com>
2024-01-03 17:57:12 -0700

    Initial commit

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+let myName = 'Luke';
+document.write(myName);//comment comment
+/*comment*/
+
+let simpleAdittion = 2+2;
+document.write(simpleAdittion);
+
+let size = 100;
+let doubleSize = size * 2;
+let minSize = (doubleSize * 2) - (size / 2);
+document.write(minSize);
